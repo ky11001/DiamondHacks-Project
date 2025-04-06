6703f92a69071556b3d313a36eac858620843772
ayxz0 <angelaz.yuxi@gmail.com>
2025-04-06 02:03:10 -0700

    im bad

--- frontend/postcss.config.mjs
+++ frontend/postcss.config.mjs
@@ -2,21 +2,4 @@ const config = {
   plugins: ["@tailwindcss/postcss"],
 };
 
-module.exports = {
-  async headers() {
-    return [
-      {
-        source: "/vibecheckLogo.webp",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=31536000, immutable", // Cache for 1 year
-          },
-        ],
-      },
-    ];
-  },
-};
-
-
 export default config;
